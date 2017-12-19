a90c5ed935d3e433b347abad7ed12fc4b55c6508
bermanboris <berman.boris@gmail.com>
2017-12-19 10:32:28 +0200

    boman refactoring

---
+++ acl.js
@@ -0,0 +1,17 @@
+import { createPermission } from '../helpers/helpers';
+import * as Errors from '../errors';
+
+export const isAdmin = createPermission({
+  error: Errors.ForbiddenError,
+  validate: user => user.isAdmin,
+});
+
+export const isNotAuthenticated = createPermission({
+  error: Errors.AlreadyAuthenticatedError,
+  validate: user => user.id,
+});
+
+export const isAuthenticated = createPermission({
+  error: Errors.UnauthorizedError,
+  validate: user => !user.id,
+});
